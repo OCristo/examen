c37f5c729092e170f95740b5bd51d383e5fbb9b3
OC <joseantoniohh03@gmail.com>
2024-03-18 10:02:27 +0100

    Creada clase Metodos donde almacenar texto y codigo repetitivo

---
+++ Metodos.java
@@ -0,0 +1,12 @@
+public class Metodos {
+    
+    final static String introduzcaPorcentaje = "Introduzca el porcentaje de aumento de salario: ";
+    final static String mostrarListaEmpleados = "Lista de Empleados:";
+
+    public static void mostrarMensaje(String mensaje){
+
+        System.out.println(mensaje);
+
+    }
+
+}
--- SistemaGestionEmpleados.java
+++ SistemaGestionEmpleados.java
@@ -6,8 +6,8 @@ public class SistemaGestionEmpleados {
      * //Crear una rama de trabajo como nombre de la rama, las siglas de tu nombre. Muévete a ella para trabajar.
     // Modifica la visibilidad a private de los atributos de la clase empleados y añade sus setters y getters. Esto implicará cambios en la clase principal para que el código sigua funcionando. 
     // Haz un commit con una frase como explicación de la mejora anterior.
-    Separa la lógica de gestión de todos los empleados en SistemaGestionEmpleados a una clase nueva llamada Empleados, distinta de la aplicación principal (main). Esta nueva clase contendrá el array de empleados, un nuevo método para mostrar el listado de los empleados código que está en el main, el método de aumentar salario y un nuevo método para dar de alta empleados que se debe usar en el main para añadir los tres empleados del ejemplo. La nueva clase Empleados contendría el atributo empleados que es el array de "Empleado", esto sería válido pero algo redundante en su uso tipo: empleados.empleados, así que se pide refactorizar el nombre del atributo empleados al nombre lista quedando al usarlo como empleados.lista. Esta nueva clase debe ser usada correctamente desde el método main para que el código siga funcionando exactamente igual.
-    Haz un commit con una frase como explicación de la mejora anterior.
+    // Separa la lógica de gestión de todos los empleados en SistemaGestionEmpleados a una clase nueva llamada Empleados, distinta de la aplicación principal (main). Esta nueva clase contendrá el array de empleados, un nuevo método para mostrar el listado de los empleados código que está en el main, el método de aumentar salario y un nuevo método para dar de alta empleados que se debe usar en el main para añadir los tres empleados del ejemplo. La nueva clase Empleados contendría el atributo empleados que es el array de "Empleado", esto sería válido pero algo redundante en su uso tipo: empleados.empleados, así que se pide refactorizar el nombre del atributo empleados al nombre lista quedando al usarlo como empleados.lista. Esta nueva clase debe ser usada correctamente desde el método main para que el código siga funcionando exactamente igual.
+    // Haz un commit con una frase como explicación de la mejora anterior.
     Crea una clase estática con todos los textos fijos (magic strings) usados en pantalla como constantes para facilitar la corrección de textos y traducciones de idiomas. Por su puesto, sustituye el uso de los strings por el uso de las constantes correspondientes.
     Haz un commit con una frase como explicación de la mejora anterior.
     Crea una nueva rama llamada Doc y muévete a ella para trabajar.
@@ -25,12 +25,12 @@ public class SistemaGestionEmpleados {
         Empleado.altaEmpleados();
 
         Scanner scanner = new Scanner(System.in);
-        System.out.print("Introduzca el porcentaje de aumento de salario: ");
+        Metodos.mostrarMensaje(Metodos.introduzcaPorcentaje);
         double porcentaje = scanner.nextDouble();
 
         Empleado.aumentarSalario(porcentaje);
         
-        System.out.println("Lista de Empleados:");
+        Metodos.mostrarMensaje(Metodos.mostrarListaEmpleados);
         for (Empleado empleado : Empleado.getLista()) {
             System.out.println(empleado);
         }
